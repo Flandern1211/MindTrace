3df65c1aa98a118a64d517a3edca5104d9337a39
Flandern1211 <3180066912wzw@gmail.com>
2026-06-06 22:24:43 +0800

    1

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,7 +3,6 @@ package app
 import (
 	searchAgent "YoudaoNoteLm/internal/agent/search"
 	"YoudaoNoteLm/internal/api"
-	"Youdao
 	"YoudaoNoteLm/internal/model/entity"
 	"YoudaoNoteLm/internal/repository"
 	"YoudaoNoteLm/internal/service"
