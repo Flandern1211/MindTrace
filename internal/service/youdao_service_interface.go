999cbd8c9d73c333f86615ae0b4c3675d689255e
Flandern1211 <3180066912wzw@gmail.com>
2026-06-10 20:10:32 +0800

    feat(service): 定义 YoudaoService 接口

---
+++ internal/service/youdao_service_interface.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"YoudaoNoteLm/internal/model/entity"
+	"YoudaoNoteLm/internal/service/external"
+)
+
+// YoudaoService 有道云笔记服务接口
+type YoudaoService interface {
+	// 绑定管理
+	Bind(userID uint, apiKey string) error
+	Unbind(userID uint) error
+	GetBinding(userID uint) (*entity.YoudaoBinding, error)
+
+	// 浏览
+	ListNotes(userID uint, folderID string) ([]external.YoudaoNoteItem, error)
+
+	// 导入
+	ImportNote(userID uint, notebookID uint, fileID string) (*entity.Source, error)
+	ImportNotesBatch(userID uint, notebookID uint, fileIDs []string) (taskID string, sourceIDs []uint, err error)
+}
